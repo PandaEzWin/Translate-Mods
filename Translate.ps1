4060cc2e77ffd0dc3eaa48ae0b4e88e1c3bb46dd
microwin7 <usa.microwin8@gmail.com>
2020-02-02 17:29:09 +0300

    Create Translate.ps1

---
+++ Translate.ps1
@@ -0,0 +1,1 @@
+curl https://github.com/microwin7/Translate-Mods/raw/master/AdvancedSolarPanel/1.7.10/ru_RU.lang | Select -ExpandProperty Content > 'src\main\resources\assets\advancedsolarpanel\lang\ru_RU.lang'
